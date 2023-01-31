e1043859aefc161e06e972cfe75c68ef26657a8b
justnikee <hk5561111212@gmail.com>
2023-02-01 00:12:37 +0530

    -

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const fs = require("fs");
 
 const habitablePlanet = [];
 
-function isHabitablePlanet(planets) {
-  return planets["koi_disposition"] === "confirmed";
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "confirmed" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
 }
 
 fs.createReadStream("cumulative_2023.01.31_09.51.36.csv")
@@ -23,6 +28,6 @@ fs.createReadStream("cumulative_2023.01.31_09.51.36.csv")
     console.log(err);
   })
   .on("end", () => {
-    console.log(result);
-    console.log("we are Done!");
+    console.log(habitablePlanet);
+    console.log(`${habitablePlanet.length} Habitable planets Found!`);
   });
